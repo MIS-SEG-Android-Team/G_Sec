e46e87d50ade28ac73a90b9bc0e8cdc01e8df490
ggutoman <ggutoman@gmail.com>
2025-02-07 10:15:59 +0800

    NFC Checkpoint #17
    fixed display selection of multiple checkpoints

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/ui/screens/settings/nfc/FragmentAddCheckpoint.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/ui/screens/settings/nfc/FragmentAddCheckpoint.java
@@ -126,15 +126,17 @@ public class FragmentAddCheckpoint extends Fragment {
                     }
                 }
 
-                if (branchExist) { break; }
+                if (!branchExist) {
 
-                branchNames.add(branch);
+                    branchNames.add(branch);
+                }
+
             }
 
             binding.tieBranchName.setAdapter(new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, branchNames.toArray()));
             binding.tieBranchName.setOnItemClickListener((parent, view, position, id) -> {
-                for (int x = 0; x < branchNames.size(); x ++) {
-                    if (binding.tieBranchName.getText().toString().equalsIgnoreCase(branchNames.get(x))) {
+                for (int x = 0; x < warehouses.size(); x ++) {
+                    if (binding.tieBranchName.getText().toString().equalsIgnoreCase(warehouses.get(x).getsBranchNm())) {
                         mViewModel.setBranch(warehouses.get(x).getSBranchCd());
                         break;
                     }
