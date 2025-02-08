3c9166f85c5e1a6395507b1c466bd374abf53d7c
ggutoman <ggutoman@gmail.com>
2025-02-08 14:47:32 +0800

    Tag NFC #19
    Fixed bug tagging schedule
    
    fix room query getting next schedule
    
    Warehouse Checkpoint #18
    
    Fixed bug displaying checkpoint created for selected warehouse

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/data/room/patrol/schedule/PatrolScheduleDao.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/data/room/patrol/schedule/PatrolScheduleDao.java
@@ -23,7 +23,7 @@ public interface PatrolScheduleDao {
             "WHERE a.schedIDxx = b.schedIDxx " +
             "AND b.dTimexxxx >= :dTimex " +
             "AND b.cRequestxx IN ('0', '1') " +
-            "ORDER BY b.dTimexxxx DESC LIMIT 1")
+            "ORDER BY b.dTimexxxx ASC LIMIT 1")
     CacheSchedule getNextCacheSchedule(String dTimex);
 
     @Query("SELECT b.dTimexxxx FROM patrol_route a, patrol_schedule b " +
